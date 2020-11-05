73c30271656d98b2c1ca2e0a1b4710074a158ecc
Kristen Stoeckeler <kstoeckeler@gmail.com>
2020-11-05 12:33:44 -0600

    setup Calculator component

---
+++ src/components/Calculator/Calculator.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+//will use these for calculator form
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+
+
+class Calculator extends Component {
+    render() {
+        return(
+            <>
+            <h1>Here is Calculator</h1>
+            </>
+        );
+    }
+}
+
+
+
+
+export default Calculator;

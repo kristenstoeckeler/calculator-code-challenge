94608b880ca111e7d3eb59a3f7fc14368843654b
Kristen Stoeckeler <kstoeckeler@gmail.com>
2020-11-05 15:06:07 -0600

    queried the pool . POST working

--- server/routes/calculation.router.js
+++ server/routes/calculation.router.js
@@ -4,22 +4,21 @@ const router = express.Router();
 
 
 
-/* ROUTES HERE */
+
 router.post('/', async (req, res) => {
   console.log("made it to server calculation router POST", req.body);
-//   const queryText =
-//     'INSERT INTO "project" ("user_id", "project_name") VALUES ($1, $2) RETURNING id';
-//   pool
-//     .query(queryText, [req.user.id, req.body.name])
-//     .then((result) => {
-//       console.log("Added new project to database");
-//       res.send(result.rows[0]);
-//       // res.sendStatus(201);
-//     })
-//     .catch((error) => {
-//       console.log("Error on project router PUT");
-//       res.sendStatus(500);
-//     });
+  const queryText =
+    'INSERT INTO "calculation" ("val1", "val2", "operator", "result") VALUES ($1, $2, $3, $4);';
+  pool
+    .query(queryText, [req.body.state.valOne, req.body.state.valTwo, req.body.state.operator, req.body.result])
+    .then((result) => {
+      console.log("Added calculation to database");
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log("Error on calculation router POST");
+      res.sendStatus(500);
+    });
 });
 
 

31a0365948eeaee3901706bffa3879499b14c040
Kristen Stoeckeler <kstoeckeler@gmail.com>
2020-11-05 12:50:07 -0600

    basic interface for calculator

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,4 +1,7 @@
+//importing React and Component for class Component 
 import React, { Component } from "react";
+
+//importing Connect for retrieving reduxStore on state
 import { connect } from "react-redux";
 
 //will use these for calculator form
@@ -7,19 +10,75 @@ import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 
+import './Calculator.css';
 
 
 class Calculator extends Component {
-    render() {
-        return(
-            <>
-            <h1>Here is Calculator</h1>
-            </>
-        );
-    }
+    state = {
+        valOne: 0,
+        valTwo: 0,
+        operator: "?",
+        result: 0,
+    };
+
+
+
+  render() {
+    return (
+      <>
+        <h1>Here is Calculator</h1>
+        <Form className="button">
+          <Row>
+            <Col>
+              <Button
+                variant="primary"
+              >
+                +
+              </Button>
+            </Col>
+            <Col>
+              <Button
+                variant="primary"
+              >
+                -
+              </Button>
+            </Col>
+            <Col>
+              <Button
+                variant="primary"
+              >
+                *
+              </Button>
+            </Col>
+            <Col>
+              <Button
+                variant="primary"
+              >
+                /
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+        <Form className="inputs">
+          <Row>
+            <Col>
+              <Form.Control/>
+            </Col>
+            <Col>
+              <Form.Control/>
+            </Col>
+          </Row>
+        </Form>
+
+        <Form className="button">
+          <Button variant="primary">
+            Submit
+          </Button>
+        </Form>
+      </>
+    );
+  }
 }
-
-
 
 
 export default Calculator;
